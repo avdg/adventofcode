2af382e6e23ce2677e72ea61b1ef762465ec50fe
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2015-12-08 00:02:35 +0100

    Solve bonus puzzle for day 1

--- day1.js
+++ day1.js
@@ -8,13 +8,22 @@ var rl = readline.createInterface({
 
 rl.on('line', function(line) {
     let floor = 0;
+    let basementPos;
     for(let i = 0; i < line.length; i++) {
         if (line[i] === "(") {
             floor++;
         } else if (line[i] === ")") {
             floor--;
+
+            if (floor === -1 && basementPos === undefined) {
+                basementPos = i + 1;
+            }
         }
     }
 
-    console.log(floor);
+    console.log("Santa is at " + floor);
+
+    if (basementPos !== undefined) {
+        console.log("Instruction " + basementPos + " took santa to basement");
+    }
 });
