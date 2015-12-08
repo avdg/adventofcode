bc59bbfa7b2c7b991d2a99fe5df0cd47a927b13d
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2015-12-08 16:07:13 +0100

    Solve bonus puzzle for day 5

--- day5.js
+++ day5.js
@@ -8,6 +8,7 @@ let rl = readline.createInterface({
 
 let total = 0;
 let good = 0;
+let evenBetter = 0;
 
 rl.on('line', function(line) {
     const vowels = "aeiou";
@@ -16,6 +17,8 @@ rl.on('line', function(line) {
     let vowelCount = 0;
     let doubleCount = 0;
     let blackListCount = 0;
+    let surroundedCount = 0;
+    let doubleRepetitionCount = 0;
 
     // Do sliding window
     for (let i = 0; i < line.length - 1; i++) {
@@ -34,6 +37,24 @@ rl.on('line', function(line) {
         if (line[i] === line[i + 1]) {
             doubleCount++;
         }
+
+        // Newer model accepts letters surrounded by the same character
+        if (i > 0 && line[i - 1] === line[i + 1]) {
+            surroundedCount++;
+        }
+
+        // Newer model accepts two letter repetitions without overlap
+        let p = line.indexOf(substr);
+
+        // Check overlap
+        if (p === i - 1 || p === i || p === i + 1) {
+            p = line.indexOf(substr, i + 2);
+        }
+
+        // Update double two pair match counter if necessary
+        if (p >= 0) {
+            doubleRepetitionCount++;
+        }
     }
 
     // Check last character, so we checked all characters
@@ -45,11 +66,19 @@ rl.on('line', function(line) {
     // Update counter
     total++;
 
+    // First model
     if (vowelCount >= 3 && doubleCount > 0 && blackListCount === 0) {
         good++;
+    }
+
+    // Updated model
+    if (doubleRepetitionCount > 0 && surroundedCount > 0) {
+        evenBetter++;
     }
 });
 
 rl.on('close', function() {
-    console.log("Found " + total + " number of strings where " + good + " are nice");
+    console.log("Found " + total + " number of strings where....");
+    console.log("* " + good + " are nice given model 1");
+    console.log("* " + evenBetter+ " are nice given model 2");
 });
