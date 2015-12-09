2260aea131452c6fa994bb5f9382b347c921256b
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2015-12-09 01:25:03 +0100

    Day 6

---
+++ day6.js
@@ -0,0 +1,98 @@
+"use strict";
+
+let readline = require('readline');
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let grid = [];
+let lightOn = 0;
+let set = (setOn, x, y) => {
+    if (grid[x] === undefined) {
+        grid[x] = [];
+    }
+
+    if (setOn) {
+        if (grid[x][y] !== true) {
+            lightOn++;
+        }
+        grid[x][y] = true;
+    } else {
+        if (grid[x][y] === true) {
+            lightOn--;
+        }
+        grid[x][y] = false;
+    }
+};
+
+let toggle = (x, y) => {
+    if (grid[x] === undefined) {
+        grid[x] = [];
+    }
+
+    if (grid[x][y] === undefined || grid[x][y] === false) {
+        lightOn++;
+        grid[x][y] = true;
+    } else {
+        grid[x][y] = false;
+        lightOn--;
+    }
+};
+
+let update = (instruction, x1, y1, x2, y2) => {
+    let tmp;
+    let instructions = {
+        "turn on": (x, y) => set(true, x, y),
+        "turn off": (x, y) => set(false, x, y),
+        "toggle": toggle
+    };
+
+    let f = instructions[instruction];
+
+    if (x1 > x2) {
+        tmp = x1;
+        x1 = x2;
+        x2 = tmp;
+    }
+
+    if (y1 > y2) {
+        tmp = y1;
+        y1 = y2;
+        y2 = tmp;
+    }
+
+    if (typeof f !== "function") {
+        console.log("Cannot execute " + instruction);
+        return;
+    }
+
+    // Update grid
+    for (let x = x1; x <= x2; x++) {
+        for (let y = y1; y <= y2; y++) {
+            f(x, y);
+        }
+    }
+};
+
+rl.on('line', function(line) {
+    // Split command
+    let commands = line.split(/\s(?=[0-9])/, 3);
+    let instruction = commands[0];
+    let locations = commands[1].split(" through", 1);
+    locations.push(commands[2]);
+
+    // Gather locations
+    locations = locations.map((l) => (l.split(",").map(Number)));
+    let x1 = locations[0][0];
+    let y1 = locations[0][1];
+    let x2 = locations[1][0];
+    let y2 = locations[1][1];
+
+    // Execute
+    update(instruction, x1, y1, x2, y2);
+});
+
+rl.on('close', function() {
+    console.log(lightOn + " lights are now on");
+});
